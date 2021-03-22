5813f8beae1b379a44b1fc0ca65346a1d897f23d
Colin Cross <crosstco@gmail.com>
2021-03-22 13:29:38 -0500

    Config file and basic spaghetti commands

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+const Discord = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const Discord = require('discord.js');
+const { prefix } = require('./config.json');
+
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready!');
+});
+
+client.on('message', (message) => {
+    if (message.content.startsWith(`${prefix}ping`)) {
+        message.channel.send('pong.');
+    } else if (message.content.startsWith(`${prefix}beep`)) {
+        message.channel.send('boop.');
+    } else if (message.content === `${prefix}server`) {
+        message.channel.send(
+            `This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`
+        );
+    } else if (message.content === `${prefix}user-info`) {
+        message.channel.send(
+            `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
+        );
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
