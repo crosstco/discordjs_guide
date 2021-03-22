3c20b27b02e0b8d0cd9310298027cb3411241b49
Colin Cross <crosstco@gmail.com>
2021-03-22 17:40:08 -0500

    Command cooldowns

--- commands/fun/pings.js
+++ commands/fun/pings.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: 'ping',
+    cooldown: 5,
     description: 'Ping!',
     execute(message, args) {
-        message.channel.send('Pong.');
+        message.channel.send('pong.');
     },
 };
