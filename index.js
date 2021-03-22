d0794884010f068857f75dac6ab5083522a1a411
Colin Cross <crosstco@gmail.com>
2021-03-22 14:06:05 -0500

    Various commands for hands on learning experience

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ client.once('ready', () => {
 });
 
 client.on('message', (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    console.log(command);
+    console.log(args);
+
     if (message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('pong.');
     } else if (message.content.startsWith(`${prefix}beep`)) {
@@ -23,6 +31,62 @@ client.on('message', (message) => {
         message.channel.send(
             `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
         );
+    } else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(
+                `You didn't provide any arguments, ${message.author}!`
+            );
+        } else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    } else if (command === 'kick') {
+        if (!message.mentions.users.size) {
+            return message.reply(
+                'you need to tag a user in order to kick them!'
+            );
+        }
+        const taggedUser = message.mentions.users.first();
+
+        message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    } else if (command === 'avatar') {
+        if (!message.mentions.users.size) {
+            return message.channel.send(
+                `Your avatar: <${message.author.displayAvatarURL({
+                    format: 'png',
+                    dynamic: true,
+                })}>`
+            );
+        }
+
+        const avatarList = message.mentions.users.map((user) => {
+            return `${user.username}'s avatar: <${user.displayAvatarURL({
+                format: 'png',
+                dynamic: true,
+            })}>`;
+        });
+
+        // send the entire array of strings as a message
+        // by default, discord.js will `.join()` the array with `\n`
+        message.channel.send(avatarList);
+    } else if (command === 'prune') {
+        const amount = parseInt(args[0]);
+
+        if (isNaN(amount)) {
+            return message.reply("that doesn't seem to be a valid number.");
+        } else if (amount < 2 || amount > 100) {
+            return message.reply(
+                'you need to input a number between 2 and 100.'
+            );
+        }
+
+        message.channel.bulkDelete(amount, true).catch((err) => {
+            console.error(err);
+            message.channel.send(
+                'there was an error trying to prune messages in this channel!'
+            );
+        });
     }
 });
 
